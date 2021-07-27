7747585a6a82ec9a1599ecf85c72492750492e4e
Deividas <dgineits@reiz.tech>
2021-07-27 09:54:40 +0300

    Complete basic look of website

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -3,9 +3,24 @@ import heading from "../../components/Heading";
 
 export default function homePage() {
   const container = document.createElement("main");
-  container.classList.add("main-container");
+  const mainHeading = heading("tacos");
+  const mainHeading2 = heading("tacos");
+  const mainHeading3 = heading("tacos");
+  const desc = document.createElement("p");
 
-  container.appendChild(heading("taco"));
+  container.classList.add("home-page");
+  mainHeading.classList.add("home-page__heading");
+  mainHeading2.classList.add("home-page__heading");
+  mainHeading3.classList.add("home-page__heading");
+  desc.classList.add("home-page__desc");
+
+  desc.innerText =
+    "Try our world famous tacos and experience a whole new world of flavour";
+
+  container.appendChild(mainHeading);
+  container.appendChild(mainHeading2);
+  container.appendChild(mainHeading3);
+  container.appendChild(desc);
 
   return container;
 }
