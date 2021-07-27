55c816045837f0ac98cdc644e4b88463e7ac4bdc
Deividas <dgineits@reiz.tech>
2021-07-27 09:54:03 +0300

    Complete basic look of website

--- src/pages/ContactPage/index.js
+++ src/pages/ContactPage/index.js
@@ -1,15 +1,23 @@
-import heading from '../../components/Heading';
+import heading from "../../components/Heading";
 import "./contactPageStyles.scss";
-
 
 export default function contactPage() {
   const container = document.createElement("section");
-  container.classList.add("contact-page");
-
   const mainHeading = heading("Contact");
+  const address = document.createElement("p");
+  const email = document.createElement("p");
+
+  container.classList.add("contact-page");
   mainHeading.classList.add("contact-page__heading");
-  
-  container.appendChild(mainHeading)
+  address.classList.add("contact-page__info");
+  email.classList.add("contact-page__email");
+
+  address.innerText = "Address: coming to town near you";
+  email.innerText = "Email: info@pinkhombre.com";
+
+  container.appendChild(mainHeading);
+  container.appendChild(address);
+  container.appendChild(email);
 
   return container;
 }
